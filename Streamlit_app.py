71d7ebcc44b089fb0276c23a3970443b78c6bcd1
seemasateesh <109935797+seemasateesh@users.noreply.github.com>
2022-07-27 22:00:19 -0400

    Update Streamlit_app.py
    Copy the Jsonformat response to normalize and Dataframe creation

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -27,3 +27,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+
+# Copy the Jsonformat response to normalize 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Creating dataframe for fruityvice_normalized
+streamlit.dataframe(fruityvice_normalized)
