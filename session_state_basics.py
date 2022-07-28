9dc90a0f7de20e389b4c636eeaf373fc1876dcc0
seemasateesh <109935797+seemasateesh@users.noreply.github.com>
2022-07-27 23:45:07 -0400

    Update session_state_basics.py

--- session_state_basics.py
+++ session_state_basics.py
@@ -9,3 +9,9 @@ if 'key' not in st.session_state:
 if 'key' not in st.session_state:
     st.session_state.key = 'value'
     
+# Delete a single key-value pair
+del st.session_state[key]
+
+# Delete all the items in Session state
+for key in st.session_state.keys():
+    del st.session_state[key]
