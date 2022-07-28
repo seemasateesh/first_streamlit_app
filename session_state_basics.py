dd6a6604662b87b53ec16a9667f8bd5eab629675
seemasateesh <109935797+seemasateesh@users.noreply.github.com>
2022-07-27 23:43:44 -0400

    Update session_state_basics.py

--- session_state_basics.py
+++ session_state_basics.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
-# Delete a single key-value pair
-del st.session_state[key]
 
-# Delete all the items in Session state
-for key in st.session_state.keys():
-    del st.session_state[key]
+# Initialization
+if 'key' not in st.session_state:
+    st.session_state['key'] = 'value'
+
+# Session State also supports attribute based syntax
+if 'key' not in st.session_state:
+    st.session_state.key = 'value'
+    
