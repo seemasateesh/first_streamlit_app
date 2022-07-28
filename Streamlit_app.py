fe6ad3b3d54fc4b9584a4cdafaad783d36221600
seemasateesh <109935797+seemasateesh@users.noreply.github.com>
2022-07-28 01:24:48 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_row)
 
 
-my_cur.execute(" insert into fruit_load_list values  (select 'streamlit') ")
+my_cur.execute(" insert into fruit_load_list values  (from 'streamlit') ")
 #my_cnx.commit()
 
 # Allow end user to add a fruit to the list
