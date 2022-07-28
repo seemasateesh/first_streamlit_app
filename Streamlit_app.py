5ab38ba3062d96fda1d82be53cb4e0fa3f3fcc24
seemasateesh <109935797+seemasateesh@users.noreply.github.com>
2022-07-27 22:06:15 -0400

    Update Streamlit_app.py
    Added fruitchoice option to the response request

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Copy the Jsonformat response to normalize 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
